6e626d5613eda67cbf403f28ae3d54e1bceda144
Peng <peng.meng@intel.com>
2017-01-24 11:08:02 +0800

    test push

--- run_realDataVLOR.sh
+++ run_realDataVLOR.sh
@@ -4,4 +4,4 @@ spark-submit \
        --master spark://sr576:7077 \
        --class org.apache.spark.ml.example.VLORRealDataExample \
        target/spark-vlbfgs-0.1-SNAPSHOT.jar \
-       1.0 10 100 5000 0.1 
+       1.0 10 100 5000 0.22 
